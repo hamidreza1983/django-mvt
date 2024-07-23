748f66fb399d30166a483ec452cec839e3c95b87
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2024-07-23 17:58:05 +0330

    model and database

--- course/admin.py
+++ course/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Serivces
-
-admin.site.register(Serivces)
 
 # Register your models here.
--- root/views.py
+++ root/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Serivces
 
 
 
 def home(request):
-    return render(request, 'root/index.html')
+    #services = Serivces.objects.all()
+    services = Serivces.objects.filter(status=True)
+    return render(request, 'root/index.html', context={"services": services})
 
 def contact(request):
     return render(request, 'root/contact.html')
