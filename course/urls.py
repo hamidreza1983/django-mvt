02fa6e5211af0f6cdfb62ffd6d604bb2279a2e3b
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2024-08-06 18:20:47 +0330

    edit view

--- course/urls.py
+++ course/urls.py
@@ -6,5 +6,9 @@ app_name = "course"
 
 urlpatterns = [
     path("", course, name="courses"),
+    path("category/<str:catname>", course, name="courses_category"),
+    path("trainer/<str:tr>", course, name="courses_trainer"),
+    path("price/<str:pr>", course, name="course_price"),
+    path("detail/<int:id>", course_detail, name="course_detail"),
     path("trainer", trainer, name="trainers"),
 ]
--- course/views.py
+++ course/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Courses, Trainer
 
 # Create your views here.
 
 
-def course(request):
-    if request.GET.get("catname"):
-        courses = Courses.objects.filter(category__name=request.GET.get("catname"))
+def course(request, **kwargs):
+    if request.GET.get('search') is not None:
+        courses = Courses.objects.filter(content__contains=request.GET.get('search'))
+    elif kwargs.get('tr') is not None:
+        courses = Courses.objects.filter(trainer__user__username=kwargs.get('tr'))
+    elif kwargs.get('catname') is not None:
+        courses = Courses.objects.filter(category__name=kwargs.get('catname')) 
+    elif kwargs.get('pr') is not None:
+        courses = Courses.objects.filter(fee__lte=float(kwargs.get('pr')))
     else:
         courses = Courses.objects.filter(status=True)
     context = {
         "courses" : courses
     }
     return render(request, 'course/courses.html', context=context)
+
+
+def course_detail(request, id):
+    course = get_object_or_404(Courses, id=id)
+    context = {
+        "course" : course
+    }
+    return render(request, 'course/course-details.html', context=context)
 
 
 def trainer(request):
